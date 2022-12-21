30a0901fb90cb6fbf065fd92b46fe35b70c471f1
code_doggy <1178297803@qq.com>
2022-12-21 21:47:18 +0800

    Tensor v1.1

---
+++ include/basic/type_tool.h
@@ -0,0 +1,37 @@
+#ifndef TYPE_TOOL_H
+#define TYPE_TOOL_H
+
+#include <string>
+#include <cstdlib>
+#if defined(__GNUC__) || defined(__clang__)
+#include <cxxabi.h>
+#endif
+
+namespace dl{
+
+template<typename T>
+std::string type_name(){
+  const char *name = typeid(T).name();
+#if defined(__GNUC__) || defined(__clang__)
+  int status;
+  char *p = abi::__cxa_demangle(name, 0, 0, &status);
+  std::string s = p;
+  free(p);
+#else
+  std::string s = name;
+#endif
+  if(std::is_const_v<std::remove_reference_t<T>>)
+    s += " const";
+  if(std::is_volatile_v<std::remove_reference_t<T>>)
+    s += " volatile";
+  if(std::is_lvalue_reference_v<T>)
+    s += " &";
+  if(std::is_rvalue_reference_v<T>)
+    s += " &&";
+  return s;
+}
+
+}
+
+
+#endif
