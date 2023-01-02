7b9a66694f6021d980e25905d30540f0680bee78
code_doggy <1178297803@qq.com>
2023-01-02 16:46:53 +0800

    Fix Tensor parallel bug(if the shape if odd)

---
+++ include/basic/tensor_macro.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace dl{
+  enum{PLUS, MINUS, MULTIPLY, DIVIDE, MOD};  // for calculator
+
+  enum{SUM, MEAN, MAX, MIN};                 // operator for Tensor
+
+  enum{NvsN, Nvs1};
+
+  enum{COL, ROW, CHANNEL};                   // axis 0, 1, 2
+
+  // enum{ARTHMATIC_F, ARTHMATIC_S, SUMUP, AVERAGE, GETMAX, GETMIN};
+
+  const int BOOST_ROW = 1 << 0;
+  const int BOOST_CHANNEL = 1 << 0;
+  #define NTHREAD_C(ncpu) (ncpu * 1)
+  #define NTHREAD_R(ncpu) (ncpu * 1)
+}
